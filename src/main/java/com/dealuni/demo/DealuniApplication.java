842e62ae4d90538405bf5054f7f3dada67b0bcbd
RuslanAlk <ruslan6al@gmail.com>
2025-05-26 13:00:31 +0300

    first commit

---
+++ src/main/java/com/dealuni/demo/DealuniApplication.java
@@ -0,0 +1,13 @@
+package com.dealuni.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealuniApplication.class, args);
+	}
+
+}
