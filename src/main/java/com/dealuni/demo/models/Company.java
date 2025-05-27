097d08d5f0f2e13e494e6626425cb202fe5679cb
RuslanAlk <ruslan6al@gmail.com>
2025-05-27 15:42:56 +0300

    Am creat modelul Company si am realizat si relatia intre Company si Discount.

---
+++ src/main/java/com/dealuni/demo/models/Company.java
@@ -0,0 +1,69 @@
+package com.dealuni.demo.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Pattern(
+            regexp = "^[A-ZĂÂÎȘȚ0-9][a-zăâîșțA-ZĂÂÎȘȚ0-9\\-\\.\\&\\,\\(\\) ]{1,99}$",
+            message = "Numele companiei trebuie să înceapă cu literă sau cifră și poate conține doar litere, cifre, " +
+                    "cratimă, punct, virgulă, & sau paranteze"
+    )
+    @Column(nullable = false, length = 100)
+    @NotNull(message = "Numele companiei este obligatoriu")
+    private String name;
+
+
+    @Pattern(
+            regexp = "^[\\p{L}0-9.,!?%:;\"'()\\-\\/\\s]{10,600}$",
+            message = "Descrierea trebuie să conțină între 10 și 600 de caractere și poate include litere, cifre, " +
+                    "spații și semne de punctuație uzuale"
+    )
+    @Column(length = 600)
+    @NotNull(message = "Descrierea este obligatorie")
+    private String description;
+
+    @Column(nullable = false, length = 200)
+    @NotNull(message = "Logo-ul companiei este obligatoriu")
+    private String logo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
--- src/main/java/com/dealuni/demo/models/Discount.java
+++ src/main/java/com/dealuni/demo/models/Discount.java
@@ -17,14 +17,16 @@ public class Discount {
 
     @Pattern(
             regexp = "^[\\p{L}0-9 .,!%&()\\-]{3,150}$",
-            message = "Numele discountului trebuie să conțină între 3 și 150 de caractere: litere, cifre, spații și simboluri uzuale (.,!%&()-)"
+            message = "Numele discountului trebuie să conțină între 3 și 150 de caractere: litere, cifre, spații și " +
+                    "simboluri uzuale (.,!%&()-)"
     )
     @Column(nullable = false, length = 150)
     private String name;
 
     @Pattern(
             regexp = "^[\\p{L}0-9.,!?%:;\"'()\\-\\/\\s]{10,600}$",
-            message = "Descrierea trebuie să conțină între 10 și 600 de caractere și poate include litere, cifre, spații și semne de punctuație uzuale"
+            message = "Descrierea trebuie să conțină între 10 și 600 de caractere și poate include litere, cifre, " +
+                    "spații și semne de punctuație uzuale"
     )
     @Column(nullable = false, length = 600)
     private String description;
@@ -55,12 +57,106 @@ public class Discount {
     private Category category;
 
     @Column(nullable = false, length = 200)
+    @NotNull(message = "Logo-ul companiei este obligatoriu")
     private String logo;
 
     @Column(length = 20)
     private String code;
 
+    //un discount poate sa fie numai de la o companie, o companie poate sa aibe multe discounturi
+    @ManyToOne(fetch = FetchType.EAGER)
+    //company_id este foreign key, face referinta la id din modelul Company
+    @JoinColumn(name = "company_id", referencedColumnName = "id")
+    @NotNull(message = "ID-ul companiei nu poate fi null")
+    private Company company;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Integer percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalDate getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDate validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
 
 
