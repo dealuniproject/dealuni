ae8ca7dfd8e0735d36927669194dfb685a6d926d
RuslanAlk <ruslan6al@gmail.com>
2025-05-31 11:52:27 +0300

    Created DTO for company (Company Request and Company Response)

---
+++ src/main/java/com/dealuni/demo/dto/CompanyRequest.java
@@ -0,0 +1,63 @@
+package com.dealuni.demo.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class CompanyRequest {
+    @Pattern(
+            regexp = "^[A-ZĂÂÎȘȚ0-9][a-zăâîșțA-ZĂÂÎȘȚ0-9\\-\\.\\&\\,\\(\\) ]{1,99}$",
+            message = "Numele companiei trebuie să înceapă cu literă sau cifră și poate conține doar litere, cifre, " +
+                    "cratimă, punct, virgulă, & sau paranteze"
+    )
+    @Column(nullable = false, length = 100)
+    @NotNull(message = "Numele companiei este obligatoriu")
+    private String name;
+
+
+    @Pattern(
+            regexp = "^[\\p{L}0-9.,!?%:;\"'()\\-\\/\\s]{10,600}$",
+            message = "Descrierea trebuie să conțină între 10 și 600 de caractere și poate include litere, cifre, " +
+                    "spații și semne de punctuație uzuale"
+    )
+    @Column(length = 600)
+    @NotNull(message = "Descrierea este obligatorie")
+    private String description;
+
+    @Column(nullable = false, length = 200)
+    @NotNull(message = "Logo-ul companiei este obligatoriu")
+    private String logo;
+
+    public CompanyRequest(String name, String description, String logo) {
+        this.name = name;
+        this.description = description;
+        this.logo = logo;
+    }
+
+    public CompanyRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
---
+++ src/main/java/com/dealuni/demo/dto/CompanyResponse.java
@@ -0,0 +1,44 @@
+package com.dealuni.demo.dto;
+
+public class CompanyResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String logo;
+
+    public CompanyResponse() {
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
